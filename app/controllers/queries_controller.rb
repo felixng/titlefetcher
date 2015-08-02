6f26e8b7a409257d1fb0df14bccc3557f0759415
Felix Ng <nfelix@gmail.com>
2015-08-02 23:43:22 +0100

    param fetched

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,9 +1,24 @@
+require 'open-uri'
+
 class QueriesController < ApplicationController
   def index
   	@query = Query.new
   end
 
   def create 
-  	@query = Query.new
+  	@query = Query.new(query_params)
+  	if @query.save 
+        redirect_to '/' 
+      else 
+        render 'new' 
+     end 
+  end
+
+  def fetch(url)
+  	
+  end
+
+  def query_params 
+    params.require(:query).permit(:url) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'queries/index'
-  get 'queries/create' => 'queries#create'
+  get 'queries/create' => 'queries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
