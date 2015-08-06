dfa9abc402b62a329842d1c2a1572a4bfda2c2d2
Felix Ng <nfelix@gmail.com>
2015-08-06 22:18:32 +0100

    removed queries helper and moved functionality to controller

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,20 +1,25 @@
 require 'open-uri'
 
 class QueriesController < ApplicationController
-  def index
-  	@query = Query.new
-  end
+	def index
+		@query = Query.new
+	end
 
-  def create 
-  	@query = Query.new(query_params)
-  	if @query.save 
-        redirect_to @query.url
-      else 
-        render 'new' 
-     end 
-  end
-  
-  def query_params 
-    params.require(:query).permit(:url) 
-  end
+	def fetch_request(url)
+		redirect_to url
+	end
+
+	def create 
+		@query = Query.new(query_params)
+		if @query.save 
+	    	fetch_request(@query.url)
+	  	else 
+	    	render 'new' 
+	 	end 
+	end
+
+	
+	def query_params 
+		params.require(:query).permit(:url) 
+	end
 end
