78174c714f073b2911575f01ceed53284ec785c2
Felix Ng <nfelix@gmail.com>
2015-08-30 19:59:45 +0100

    added in paging loop

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -8,21 +8,6 @@ class QueriesController < ApplicationController
 		@query = Query.new()
 	end
 
-	def fetch_titles(url)
-	    doc = Nokogiri::HTML(open(url))
-	    titles = doc.css('h2').map
-
-	    titles.each do |title|
-	    	post = Post.new(title: title.text)
-	    	post.save
-	    end
-
-		posts = Post.order(:created_at)
-	    send_data posts.as_csv
-
-	    #render 'fetch'
-	end
-
 	def create 
 		@query = Query.new(query_params)
 		if @query.save 
@@ -32,76 +17,52 @@ class QueriesController < ApplicationController
 	 	end 
 	end
 
-	def fetch
-	  @posts = Post.order(:created_at)
-	  
-	  respond_to do |format|
-	    format.html
-	    format.csv { send_data @posts.as_csv }
-	  end
+	def fetch_titles(url)
+		page = 1
+	 
+		loop do
+			page_url = url.chomp('/') + "/page/#{page}/"
+
+			begin 
+				doc = Nokogiri::HTML(open(page_url))
+				continute = get_post_title(doc)
+			rescue OpenURI::HTTPError => e
+			  if e.message == '404 Not Found'
+			    break
+			  else
+			    raise e
+			  end
+			end 
+
+			break if continute == false
+
+			page += 1
+		end
+
+		posts = Post.order(:created_at)
+	    send_data posts.as_csv, :filename => filenamify(url)
+	end
+
+	def filenamify(url)
+		URI.parse(url).host + ".csv" 
 	end
 
 	def query_params 
 		params.require(:query).permit(:url) 
 	end
 	 
-	def get_post_details(doc)
-	  #an array of hashes with each posts title, url, date, author
-	  posts = []
-	 
-	  doc.css('h2').each do |post|
-	 
-	    details = post.xpath('.//a')[0].attributes()
-	 
-	    #get url
-	    url = details['href'].value()
-	    #verify that the url matches mashable.com and is not feedproxy.google.com
-	    next #unless url =~ /mashable.com/
-	    puts url
-	 
-	    #get and parse title
-	    title = details['title'].value() if details['title'] != nil
-	    title = title.gsub(/Permanent Link to /, '')
-	    title = CGI.escape( title ) #make sure title is encoded
-	    puts title
-	 
-	    #get published date
-	    next if post.xpath('.//time')[0] == nil #avoid 'sponsored posts'
-	 
-	    date_details = post.xpath('.//time')[0].attributes()
-	    date_string = date_details['datetime'].value()
-	    date = DateTime.parse(date_string) #creates a time object
-	    puts date.to_s
-	 
-	    #get author
-	    author_details = post.xpath('.//span')[0].children()
-	    author = author_details.children().inner_text()
-	    puts author
-	 
-	    #store the data in a hash
-	    posts << {'title' => title, 'date' => date, 'author' => author, 'url' => url}
-	  end
-	  posts #returns an array of hashes with details for each post
+	def get_post_title(doc)
+		titles = doc.css('h2').map
+
+		if (titles.count != 10)
+			false
+		else
+		    titles.each do |title|
+		    	post = Post.new(title: title.text)
+		    	post.save
+		    end
+		   	true
+		end 
 	end
-	 
-	public
-	def crawl(url)
-	  #keeps track of page number
-	  page = 1
-	 
-	  #loop through each page on Mashable, and get the author, title, url, and date for each post
-	  loop do
-	    #get post details per page
-	    url = url + "/#{page}/"
-	    doc = Nokogiri::HTML(open(url))
-	    posts = get_post_details(doc)
-	    p posts
-	 
-	    #you've crawled all the pages if the returned posts array is empty
-	    break if posts == []
-	 
-	    #move on to the next page
-	    page += 1
-	  end
-	end
+
 end
